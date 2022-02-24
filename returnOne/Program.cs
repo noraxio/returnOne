ccf328920155280edf5ec5d8ecf28e8d2878b913
devil <devil@DESKTOP-4IBVB9K>
2022-02-24 02:52:44 +0000

    Add project files.

---
+++ returnOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numberStorage = new List<int>();
+            Console.WriteLine("Enter -1 to exit");
+            Console.WriteLine("input your numbers");
+            
+
+            while (true)
+            {
+                int userInput = int.Parse (Console.ReadLine());
+                
+                Console.Write("do you want to input another number: ");
+                if(userInput == -1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("here are your Highest and Lowest numbers");
+                    break;
+
+                }
+                numberStorage.Add(userInput);
+            }
+
+            Console.WriteLine($"Highest: {numberStorage.Max()}");
+            Console.WriteLine($"Lowest: {numberStorage.Min()}");
+           
+            /*
+            for (int i = 0; i < numberStorage.Count; i++)
+            {
+                Console.WriteLine(numberStorage[i] + ", ");
+            }
+            */
+        }
+    }
+}
